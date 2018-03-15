1002aa62f5f79b80fb43ccc8491b397cafade6e0
Smiledemon <790333234@qq.com>
2018-03-15 17:14:42 +0800

    test

---
+++ test/com/xceder/Demo2Test.java
@@ -0,0 +1,22 @@
+package com.xceder;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ *
+ * @author: xiaopan
+ * Date: 2018-03-15
+ * Time: 下午5:13
+ */
+public class Demo2Test {
+
+    @Test
+    public void main() {
+        Assert.assertTrue(true);
+    }
+}
---
+++ test/com/xceder/DemoTest.java
@@ -0,0 +1,21 @@
+package com.xceder;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ *
+ * @author: xiaopan
+ * Date: 2018-03-15
+ * Time: 下午5:07
+ */
+public class DemoTest {
+
+    @Test
+    public void main() {
+        Assert.assertEquals(3,3);
+    }
+}
